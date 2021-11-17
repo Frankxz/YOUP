e7ea8f5e3e65c11140bedc0d37f550ce4fe3461c
Miller <gelaseen@gmail.com>
2021-11-18 02:33:22 +0700

    FirebaseManager as service layer

---
+++ YOUP/ViewController/Service/FirebaseManager.swift
@@ -0,0 +1,43 @@
+
+//  FireBaseManager.swift
+//  YOUP
+//
+//  Created by Robert Miller on 18.11.2021.
+//
+
+import UIKit
+import Firebase
+import FirebaseStorage
+
+class FirebaseManager {
+    
+    static let shared = FirebaseManager()
+    private init() {}
+    
+    var databaseRef: DatabaseReference = Database.database().reference(withPath: "users")
+    var storageRef: StorageReference!
+    
+    
+    func fetchUser(user: User, complition: @escaping (YoupUser)->()){
+        var youpUser: YoupUser!
+        
+        //Get youpUser
+        databaseRef = databaseRef.child(String(user.uid)).child("userInfo")
+        databaseRef.observe(.value) { snapshot in
+            youpUser = YoupUser(snapshot: snapshot)
+            print(youpUser.fullname)
+        
+            self.databaseRef = Database.database().reference(withPath: "users").child(String(user.uid))
+            self.databaseRef.observe(.value) { snapshot in
+            var bufferComments: [Comment] = []
+            for item in snapshot.childSnapshot(forPath: "comments").children{
+                let comment = Comment(snapshot: item as! DataSnapshot)
+                bufferComments.append(comment)
+            }
+            youpUser.setStats(snapshot: snapshot)
+            youpUser.comments = bufferComments
+            complition(youpUser)
+        }
+    }
+    }
+}
