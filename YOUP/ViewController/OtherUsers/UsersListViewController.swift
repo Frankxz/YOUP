5952af0f15eaaab6a698314f0a236f4ce26e37b1
Miller <gelaseen@gmail.com>
2021-12-09 00:24:25 +0700

    setup searchBar logic

--- YOUP/ViewController/OtherUsers/UsersListViewController.swift
+++ YOUP/ViewController/OtherUsers/UsersListViewController.swift
@@ -11,13 +11,18 @@ import FirebaseStorage
 class UsersListViewController: UITableViewController {
     
     var youpUsers: [YoupUser] = []
+    var filteredYoupUsers: [YoupUser] = []
     var ref: DatabaseReference!
     var storageRef: StorageReference!
     var usersImages: [String : UIImage] = [:]
     var isAllUsersFetched = false
     
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         ref = Database.database().reference(withPath: "users")
         navigationController?.navigationBar.barTintColor = UIColor(red: 11/255, green: 0, blue: 20/255, alpha: 1)
         tabBarController?.tabBar.barTintColor = UIColor(red: 11/255, green: 0, blue: 20/255, alpha: 1)
@@ -28,7 +33,6 @@ class UsersListViewController: UITableViewController {
         if !isAllUsersFetched {
             FirebaseManager.shared.fetchUsers { [self] users in
                 youpUsers = users
-                self.tableView.reloadData()
                 for user in youpUsers {
                     FirebaseManager.shared.fetchAvatar(userID: user.id) { image in
                         user.image = image
@@ -40,6 +44,7 @@ class UsersListViewController: UITableViewController {
                     if user.id == Auth.auth().currentUser?.uid {
                         youpUsers.remove(at: index)
                         print("deleted me in users list ")
+                        filteredYoupUsers = youpUsers
                         tableView.reloadData()
                     }
                 }
@@ -55,14 +60,14 @@ class UsersListViewController: UITableViewController {
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        youpUsers.count
+        filteredYoupUsers.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UserTableViewCell
         
-        let youpUser = youpUsers[indexPath.row]
+        let youpUser = filteredYoupUsers[indexPath.row]
         
         //let avatarImage = usersImages[youpUser.id] ?? UIImage(systemName: "person.circle")
         cell.configure(youpUser: youpUser,
@@ -97,5 +102,20 @@ class UsersListViewController: UITableViewController {
                 print("Data reload")
             }
         }
+    }
+}
+
+extension UsersListViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredYoupUsers = []
+        
+        if searchText == "" { filteredYoupUsers = youpUsers } else {
+            for filteredYoupUser in youpUsers {
+                if filteredYoupUser.username.lowercased().contains(searchText.lowercased()) || filteredYoupUser.fullname.lowercased().contains(searchText.lowercased()) {
+                    filteredYoupUsers.append(filteredYoupUser)
+                }
+            }
+        }
+        tableView.reloadData()
     }
 }
