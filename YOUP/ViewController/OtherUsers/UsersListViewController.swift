12bf3d384236901faefaeb4f0e5a6f38d660abed
Miller <gelaseen@gmail.com>
2021-12-09 00:30:39 +0700

    fix textfield textColor

--- YOUP/ViewController/OtherUsers/UsersListViewController.swift
+++ YOUP/ViewController/OtherUsers/UsersListViewController.swift
@@ -23,6 +23,7 @@ class UsersListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        searchBar.searchTextField.textColor = .white
         ref = Database.database().reference(withPath: "users")
         navigationController?.navigationBar.barTintColor = UIColor(red: 11/255, green: 0, blue: 20/255, alpha: 1)
         tabBarController?.tabBar.barTintColor = UIColor(red: 11/255, green: 0, blue: 20/255, alpha: 1)
